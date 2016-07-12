464f58a45b11dcf87b56ff67f73705538517d587
Paul Campbell <nimbleltd@gmail.com>
2016-07-12 07:00:02 -0500

    added table elements and connected WeatherList to redux via mapStateToProps, this closes #12

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,15 +1,18 @@
 import React, {Component} from 'react';
-// import {connect} from 'react-redux';
+import {connect} from 'react-redux';
 // import {bindActionCreators} from 'redux';
 // import {fetchWeather} from '../actions/index';
 
-export default class WeatherList extends Component {
+class WeatherList extends Component {
 	render () {
 		return (
 			<table className="table table-hover">
 				<thead>
 					<tr>
 						<th>City</th>
+						<th>Temp</th>
+						<th>Pressure</th>
+						<th>Humidity</th>
 					</tr>
 				</thead>
 					<tbody>
@@ -18,3 +21,13 @@ export default class WeatherList extends Component {
 		)
 	}
 }
+
+// function mapStateToProps(state) {
+// 	return {weather: state.weather};
+// }
+// the following equals what is above via ES6
+function mapStateToProps({weather}) {
+	return {weather};
+}
+
+export default connect(mapStateToProps)(WeatherList);
